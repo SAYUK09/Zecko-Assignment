b44abad1c99cf2c7d1463cbcc68637ac36e72df2
Sayuri <sayurikamble123@gmail.com>
2022-09-01 21:01:07 +0530

    OAuth Setup and Write to the sheet

--- script.js
+++ script.js
@@ -1,22 +1,98 @@
-const API_KEY = "AIzaSyBUtLChhGPGARgiohFzb7vSGExjmc1EKHs";
+const spreadsheetId = "1pQnWNDfzV1m4OrDFhslVoGdeN_Xm5Mu_gFAN9GE13Pk";
 const DISCOVERY_DOC =
   "https://sheets.googleapis.com/$discovery/rest?version=v4";
+const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
 
-(async () => {
-  fetch("https://apis.google.com/js/api.js").then(() => console.log("akakak"));
-})();
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
 
-document.getElementById("initBtn").addEventListener("click", () => {
+document.getElementById("authorize_button").style.visibility = "hidden";
+document.getElementById("signout_button").style.visibility = "hidden";
+
+async function getApiKey() {
+  const res = await fetch("/config.json");
+  const data = await res.json();
+  const API_KEY = data.API_KEY;
+
+  return API_KEY;
+}
+
+async function getClientId() {
+  const res = await fetch("/config.json");
+  const data = await res.json();
+  const CLIENT_ID = data.CLIENT_ID;
+
+  return CLIENT_ID;
+}
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
   gapi.load("client", intializeGapiClient);
-});
+}
 
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
 async function intializeGapiClient() {
   await gapi.client.init({
-    apiKey: API_KEY,
+    apiKey: await getApiKey(),
     discoveryDocs: [DISCOVERY_DOC],
   });
+  gapiInited = true;
+  maybeEnableButtons();
+}
 
-  listMajors();
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+async function gisLoaded() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: await getClientId(),
+    scope: SCOPES,
+    callback: "", // defined later
+  });
+  gisInited = true;
+  maybeEnableButtons();
+}
+
+function maybeEnableButtons() {
+  if (gapiInited && gisInited) {
+    document.getElementById("authorize_button").style.visibility = "visible";
+  }
+}
+
+function handleAuthClick() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw resp;
+    }
+    document.getElementById("signout_button").style.visibility = "visible";
+    document.getElementById("authorize_button").innerText = "Refresh";
+    await listMajors();
+  };
+
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({ prompt: "consent" });
+  } else {
+    tokenClient.requestAccessToken({ prompt: "" });
+  }
+}
+
+function handleSignoutClick() {
+  const token = gapi.client.getToken();
+  if (token !== null) {
+    google.accounts.oauth2.revoke(token.access_token);
+    gapi.client.setToken("");
+    document.getElementById("content").innerText = "";
+    document.getElementById("authorize_button").innerText = "Authorize";
+    document.getElementById("signout_button").style.visibility = "hidden";
+  }
 }
 
 async function listMajors() {
@@ -24,11 +100,13 @@ async function listMajors() {
   console.log("tral");
   try {
     response = await gapi.client.sheets.spreadsheets.values.get({
-      spreadsheetId: "1pQnWNDfzV1m4OrDFhslVoGdeN_Xm5Mu_gFAN9GE13Pk",
-      range: "A1",
+      spreadsheetId: spreadsheetId,
+      range: "A:A",
     });
 
-    console.log(response, "ress");
+    const data = JSON.parse(response.body);
+
+    analyze(data.values);
   } catch (err) {
     console.log("err", err);
     document.getElementById("content").innerText = err.message;
@@ -39,4 +117,17 @@ async function listMajors() {
     document.getElementById("content").innerText = "No values found.";
     return;
   }
+}
+
+async function analyze(data) {
+  const values = data.map((website) => {
+    if (website.toString().toLowerCase() === "website".toLowerCase()) {
+      console.log("tata");
+      return "Category";
+    } else {
+      return "baka";
+    }
+  });
+
+  return values;
 }
